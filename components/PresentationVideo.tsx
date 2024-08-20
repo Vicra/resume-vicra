f215c4a169e8bcba9de4e404310576b43ac4d717
Victor Ramirez <honvramirez@gmail.com>
2024-08-19 21:16:45 -0600

    Update PresentationVideo.tsx

--- components/PresentationVideo.tsx
+++ components/PresentationVideo.tsx
@@ -2,7 +2,7 @@ export function PresentationVideo() {
     return (
         <div className="flex justify-center p-12">
             <div>
-                <iframe width="1024" height="512" src="https://www.youtube.com/embed/5bVIwzNoeeI?si=gldSLbS4Pwx2iVYY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <iframe width="1024" height="512" src="https://www.youtube.com/embed/5bVIwzNoeeI?si=gldSLbS4Pwx2iVYY" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
             </div>
         </div>
     )
